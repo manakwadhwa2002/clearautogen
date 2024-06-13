334704d9ce7b607c3a32148549c2882c493ae693
Manak Wadhwa <manakwadhwa2002@gmail.com>
2024-06-13 11:11:40 +0530

    Updates

---
+++ precache-manifest.3f165021b242a410022dcf72c3ac207c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "734a11e18cd0708617c9e34f0a58249e",
+    "url": "/clearautogen/index.html"
+  },
+  {
+    "revision": "c7d2f2b251aad01d51e7",
+    "url": "/clearautogen/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "091762270205a56c46e1",
+    "url": "/clearautogen/static/js/2.2b9d955b.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/clearautogen/static/js/2.2b9d955b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96ace7e90c94049a9e2e",
+    "url": "/clearautogen/static/js/3.4a569aee.chunk.js"
+  },
+  {
+    "revision": "2e5689583f50e4f80eeb841c0442bbfd",
+    "url": "/clearautogen/static/js/3.4a569aee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3cd056382ca1a103bdf",
+    "url": "/clearautogen/static/js/4.5f07cbe4.chunk.js"
+  },
+  {
+    "revision": "b63e781656a7c21988295c1056f07347",
+    "url": "/clearautogen/static/js/4.5f07cbe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "674ba1bb6e56400e0c59",
+    "url": "/clearautogen/static/js/5.38bb039b.chunk.js"
+  },
+  {
+    "revision": "c7d2f2b251aad01d51e7",
+    "url": "/clearautogen/static/js/main.25be7ffd.chunk.js"
+  },
+  {
+    "revision": "4a66989e2bfa0b9ba537",
+    "url": "/clearautogen/static/js/runtime-main.b4b9abfa.js"
+  }
+]);
